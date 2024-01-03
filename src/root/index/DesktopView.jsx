bb9c6dc38cd7c15be17295a7e041d97def3f9c63
Anga205 <angadbhalla2005@gmail.com>
2024-01-03 12:45:32 +0530

    fixed indentation in desktop view

--- src/root/index/DesktopView.jsx
+++ src/root/index/DesktopView.jsx
@@ -1,5 +1,4 @@
 import { createEffect, createSignal } from "solid-js";
-import Carousel from "./components/Carousel";
 
 const NavbarButton = ({ text, redirect = "#" }) => {
     return (
@@ -47,8 +46,8 @@ const Header = () => {
 
     return (
         <div>
-        <h1 class="text-[3vh] font-bold">{typedText()}_</h1>
-        <h1 class="text-[7vh] font-bold">I&apos;m Anga.</h1>
+        <h1 class="text-[3vh] font-bold select-none">{typedText()}_</h1>
+        <h1 class="text-[7vh] font-bold select-none">I&apos;m Anga.</h1>
         </div>
     );
 };
@@ -67,40 +66,40 @@ const DesktopView = () => {
         return () => window.removeEventListener('load', onLoad);
     });
     return (
-        <>
-            <div className="min-h-screen w-screen h-screen text-gray-200 ">
-                <div className="bg-gradient-to-b from-black via-gray-950 to-gray-900 w-screen h-screen fixed p-10">
-                    <img className="h-full opacity-10" src="/nodes.png"/>
-                </div>
-                <nav className="fixed bg-transparent p-3 w-full slide-down bg-black bg-opacity-25">
-                    <div className="container mx-auto flex justify-between items-center">
-                        <div className="flex w-full justify-between items-center h-16">
-                            <img
-                                src="/anga.svg"
-                                className="h-full"
-                            />
-                            <div className="flex space-x-10">
-                                <NavbarButton text="Home" />
-                                <NavbarButton text="Projects" />
-                                <NavbarButton text="Resume" />
-                                <NavbarButton text="Contact" />
-                            </div>
+        <div className="min-h-screen w-screen h-screen text-gray-200">
+            <div className="bg-gradient-to-b from-black via-gray-950 to-gray-900 w-screen h-screen fixed p-10 select-none">
+                <img className="h-full opacity-10" src="/nodes.png" draggable="false"/>
+            </div>
+            <nav className="fixed bg-transparent p-3 w-full slide-down bg-black bg-opacity-25 select-none">
+                <div className="container mx-auto flex justify-between items-center">
+                    <div className="flex w-full justify-between items-center h-16">
+                        <img
+                            src="/anga.svg"
+                            className="h-full"
+                        />
+                        <div className="flex space-x-10">
+                            <NavbarButton text="Home" />
+                            <NavbarButton text="Projects" />
+                            <NavbarButton text="Resume" />
+                            <NavbarButton text="Contact" />
                         </div>
                     </div>
-                </nav>
-                <div className="flex justify-center items-center h-[100%]">
-                    <div className="px-16 max-w-7xl flex justify-center items-center space-x-20">
+                </div>
+            </nav>
+            <div className="flex justify-center items-center h-[100%]">
+                <div className="px-16 max-w-7xl flex justify-center items-center space-x-20">
                     <div className="text-start space-y-4 slide-left">
                         <Header />
                         <Yapping>I&apos;m a {Math.floor((Date.now() / 1000 - 1117704600) / (60 * 60 * 24 * 365))} year old competitive programming enthusiast, web developer and open-source contributer. I&apos;ve done it all, from making globally scalable secure systems APIs to making the most specialized IoT prototypes.</Yapping>
                         <Yapping>I&apos;ve given TED talks, worked with some of the most enthusiastic tech startups, headed some of the biggest clubs in University, and competed in 40+ hackathons and coding contests.</Yapping>
                         <Yapping>I am also lactose intolerant.</Yapping>
                     </div>
-                    <div className="relative w-full">
+                    <div className="relative w-full select-none">
                         <img
                             src="/image-border.png"
                             alt="Background"
                             className="w-full object-cover"
+                            draggable="false"
                         />
                         
                         <div className="absolute top-1/2 left-1/2 w-full p-12 transform -translate-x-1/2 -translate-y-1/2">
@@ -108,13 +107,13 @@ const DesktopView = () => {
                             src="/me.jpg"
                             alt="Foreground"
                             className="object-cover slide-right"
+                            draggable="false"
                             />
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </>
     );
 }
 
