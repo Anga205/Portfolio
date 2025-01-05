384807d2fc104cfb6354a6e7fe7e48e29543a4bd
Anga205 <angadbhalla2005@gmail.com>
2025-01-06 00:26:47 +0530

    fixed the drawer button on mobile

--- src/root/index/components/Drawer/index.jsx
+++ src/root/index/components/Drawer/index.jsx
@@ -1,9 +1,8 @@
 import { createSignal, onCleanup, onMount } from "solid-js";
-import "./drawer.css";
 
 const DrawerButton = ({ title, link = "#", newPage = false }) => {
   return (
-    <a href={link} className="drawer-link" target={newPage ? "_blank" : "_self"}>
+    <a href={link} className="text-white no-underline my-2 text-lg" target={newPage ? "_blank" : "_self"}>
       <button className="w-full hover:bg-gray-800 p-3">
         {title}
       </button>
@@ -46,12 +45,12 @@ const Drawer = () => {
 
   onMount(() => {
     window.addEventListener("touchstart", handleTouchStart);
-    document.addEventListener("click", handleClickOutside);
+    document.addEventListener("click", handleClickOutside, true);
   });
 
   onCleanup(() => {
     window.removeEventListener("touchstart", handleTouchStart);
-    document.removeEventListener("click", handleClickOutside);
+    document.removeEventListener("click", handleClickOutside, true);
   });
 
   const ButtonList = [
@@ -76,20 +75,20 @@ const Drawer = () => {
 
   return (
     <>
-      <button onClick={toggleDrawer} className="drawer-toggle">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+      <button onClick={toggleDrawer} className="fixed top-2 left-2 bg-none border-none text-white cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
           <path d="M3 12h12M3 6h18M3 18h6"/>
         </svg>
       </button>
-      <div className={`drawer ${isOpen() ? "open" : ""}`}>
-        <nav className="drawer-nav space-y-0 h-full items-center justify-between py-40">
+      <div className={`fixed top-0 left-0 w-64 h-full bg-black bg-opacity-90 text-white transform transition-transform duration-300 ease-in-out z-50 ${isOpen() ? "translate-x-0" : "-translate-x-full"}`}>
+        <nav className="flex flex-col h-full justify-between py-10">
           <div className="flex items-center justify-center space-x-4">
             <img src="/anga.svg" className="w-1/4" />
             <h1 className="text-2xl font-extrabold">Anga</h1>
           </div>
-          <div className="space-y-0 flex flex-col pb-28 w-full">
+          <div className="flex flex-col space-y-2 pb-[100%] w-full">
             {ButtonList.map((button) => (
-              <DrawerButton title={button.title} link={button.link} newPage={button.newPage} />
+              <DrawerButton key={button.title} title={button.title} link={button.link} newPage={button.newPage} />
             ))}
           </div>
         </nav>
