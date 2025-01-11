e67ed17480489896222bfbfc3874de8b47d2098e
Anga205 <angadbhalla2005@gmail.com>
2025-01-11 14:42:13 +0530

    initialized migration

--- README.md
+++ README.md
@@ -1,35 +1,8 @@
-# Self Hosting Instructions
+# React + Vite
 
-### Step 1: Clone the Repository
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Open your terminal and run the following command to clone the repository:
+Currently, two official plugins are available:
 
-```sh
-git clone git@github.com:Anga205/anga.codes.git
-```
-
-### Step 2: Navigate to the Project Directory
-
-Change into the project directory:
-
-```sh
-cd anga.codes
-```
-
-### Step 3: Install Dependencies
-
-Install the necessary dependencies using Bun:
-
-```sh
-bun install
-```
-
-### Step 4: Start the Application
-
-Start the Bun app with the following command:
-
-```sh
-bun run dev
-```
-
-Then you should be able to access the app at `http://localhost:5173`.
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/MobileView/index.jsx
+++ src/MobileView/index.jsx
@@ -1,62 +1,17 @@
-import { createEffect, createSignal } from "solid-js";
-import Drawer from "./components/Drawer";
-import "./index.css";
-
-const Yapping = ({ yapsDone, setYapsDone, index, children }) => {
-    const [displayText, setDisplayText] = createSignal("");
-    const [stillTyping, setStillTyping] = createSignal(true);
-    const [buffer, setBuffer] = createSignal("");
-    var typingSpeed = 10;
-    var textToType = children.replace("$age", Math.floor((Date.now() / 1000 - 1117704600) / (60 * 60 * 24 * 365)))
-    var currentChar = 0;
-    createEffect(() => {
-        let typer = () => {
-            let interval = setInterval(()=>{
-                currentChar++
-                setDisplayText(textToType.slice(0, currentChar))
-                if(displayText().length === textToType.length){
-                    setDisplayText(textToType)
-                    setStillTyping(false)
-                    clearInterval(interval)
-                    setYapsDone(yapsDone() + 1)
-                }
-            }, typingSpeed)
-        }
-        let bufferModify = () => {
-            const getRandomCharacter = (str) => {
-                const randomIndex = Math.floor(Math.random() * str.length);
-                return str[randomIndex];
-            }
-            let interval = setInterval(()=>{
-                if(stillTyping()){
-                    setBuffer(getRandomCharacter("!@#$%^&*()_+-=[]{}|;/?"))
-                } else {
-                    setBuffer("")
-                    clearInterval(interval)
-                }
-            }, typingSpeed/5)
-        }
-        if (index===yapsDone()){
-            typer();
-            bufferModify();
-        }
-    })
-    return (
-        <p className="text-base md:text-xl text-gray-400">{displayText()}{buffer()}</p>
-    );
-}
-
+import { useState, useEffect } from "react";
+import "./index.css"
+import Drawer from "./components/drawer";
 
 const ImageOfMe = ({ yapsDone, setYapsDone, index }) => {
-    createEffect(() => {
+    useEffect(() => {
         const elementsToAnimate = document.querySelectorAll('.slide-left-mobile, .slide-right-mobile');
-        if (index===yapsDone()){
+        if (index===yapsDone){
             elementsToAnimate.forEach(element => {
                 element.classList.add('animate')
             });
-            setYapsDone(yapsDone() + 1)
+            setYapsDone(yapsDone + 1)
         }
-    })
+    },[yapsDone])
     return (
         <div className="relative w-full select-none">
             <img
@@ -78,16 +33,56 @@ const ImageOfMe = ({ yapsDone, setYapsDone, index }) => {
     )
 }
 
+const Yapping = ({ yapsDone, setYapsDone, index, children }) => {
+
+    const textToType = children.replace("$age", Math.floor((Date.now() / 1000 - 1117704600) / (60 * 60 * 24 * 365)))
+    let [displayText, setDisplayText] = useState("")
+    let [buffer, setBuffer] = useState("")
+    let currentChar = 0
+    const typingSpeed = 13
+
+    const updateBuffer = () => {
+        if(currentChar === textToType.length){
+            setBuffer("")
+        } else {
+            const str = "!@#$%^&*()_+-=[]{}|;/?"
+            setBuffer(str[Math.floor(Math.random() * str.length)])
+            setTimeout(updateBuffer, typingSpeed/5)            
+        }
+    }
+
+    const appendToText = () => {
+        if (currentChar === textToType.length) {
+            setDisplayText(textToType)
+            setYapsDone(yapsDone + 1)
+        } else {
+            currentChar++
+            setDisplayText(textToType.slice(0, currentChar))
+            setTimeout(appendToText, typingSpeed)
+        }
+    }
+
+    useEffect(() => {
+        if(yapsDone===index){
+            appendToText()
+            updateBuffer()
+        }
+    },[yapsDone])
+
+    return (
+        <p className="text-base md:text-xl text-gray-400">{displayText}{buffer}</p>
+    )
+}
 
 const MobileView = () => {
-    const [yapsDone, setYapsDone] = createSignal(0);
+    const [yapsDone, setYapsDone] = useState(0);
     return (
         <div className="h-screen w-screen text-gray-200">
             <div className="bg-gradient-to-b from-black via-gray-950 to-gray-900 w-screen h-screen fixed py-20 select-none z-[-1]">
                 <img className="w-full opacity-15" src="/nodes.png" draggable="false"/>
             </div>
             <Drawer />
-            <main className="flex flex-col md:flex-row justify-center items-end p-4 space-y-10">
+            <main className="flex flex-col md:flex-row justify-center items-center p-4 space-y-10">
                 <div className="text-start space-y-4 px-5 w-full">
                     <h1 className="text-3xl font-bold w-full text-start">Hello there! I'm Anga.</h1>
                     <Yapping index={0} yapsDone={yapsDone} setYapsDone={setYapsDone}>I&apos;m a $age year old competitive programming enthusiast, web developer and open-source contributor. I&apos;ve done it all, from making globally scalable secure systems APIs to making the most specialized IoT prototypes.</Yapping>
