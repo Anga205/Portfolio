9ae44cea7771b80ba7637aecfcfbcd69abbd7223
Anga205 <angadbhalla2005@gmail.com>
2025-01-18 15:27:46 +0530

    completed mobile carousel

--- src/components/cubeCarousel/index.jsx
+++ src/components/cubeCarousel/index.jsx
@@ -6,15 +6,15 @@ import 'swiper/css/pagination';
 
 import './styles.css';
 
-import { EffectCube } from 'swiper/modules';
+import { EffectCube, Pagination } from 'swiper/modules';
 
 
 const Update = (e) => {
-    console.log(e)
+    console.log(e.activeIndex)
 }
 
 const Buttons = ({ project }) => (
-    <div className="flex justify-between space-x-3 w-full select-none">
+    <div className="flex justify-between space-x-3 w-full select-none pb-4">
         <a href={project.github} target="_blank" className={`text-blue-500 ${project.link ? 'w-1/2' : 'w-full'}`}>
             <button className="flex items-center justify-center space-x-2 bg-white p-2 rounded-md w-full">
                 <div className='w-6 h-6'>
@@ -26,8 +26,8 @@ const Buttons = ({ project }) => (
         {project.link && (
             <a href={project.link} target="_blank" className='text-blue-500 w-1/2'>
                 <button className="flex items-center justify-center space-x-2 bg-white p-2 rounded-md w-full">
-                    <div className='w-6 h-6'>
-                        <img src="web.png" alt="Link" className="w-full h-full"/>
+                    <div className='w-6 h-6 aspect-square'>
+                        <img src="web.png" alt="Link" className="w-full h-full aspect-square"/>
                     </div>
                     <span className='pl-3'>Link</span>
                 </button>
@@ -49,20 +49,23 @@ return (
                 shadowOffset: 20,
                 shadowScale: 0.94,
             }}
-            modules={[EffectCube]}
+            pagination={{ 
+                clickable: true, 
+            }}
+            modules={[EffectCube, Pagination]}
             onPaginationUpdate={Update}
             className="mySwiper"
         >
             {projects.map((project, index) => (
                 <SwiperSlide key={index}>
-                    <div style={{ backgroundImage: project.gradient }} className='flex flex-col justify-between w-full h-full p-4 rounded-md border-spacing-4 border-gray-700 border-2'>
+                    <div style={{ backgroundImage: project.gradient }} className='select-none flex flex-col justify-between w-full h-full p-4 rounded-md border-spacing-4 border-gray-700 border-2'>
                         <div>
                             <h1 className="w-full text-center text-4xl font-bold">
                                 {project.name}
                             </h1>
                             <hr className='m-4'/>
                             <img src={project.image} className='border-gray-700 border-2 rounded-lg'/>
-                            <h3>{project.description}</h3>
+                            <h3 className='pt-2'>{project.description}</h3>
                         </div>
                         <Buttons project={project} />
                     </div>
